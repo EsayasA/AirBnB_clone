e257ce7ec4c016e6bd0ba97ed2c8b72561852118
EsayasA <esayasaregawi29@gmail.com>
2023-12-09 04:23:13 -0800

    airbnb

---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Represent User class."""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Represent a User.
+
+    Attributes:
+        email (str): email.
+        password (str): password.
+        first_name (str): first name.
+        last_name (str): last name.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
